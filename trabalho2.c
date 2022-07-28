32c38b77cc76c98526d3a873900bc10a9fccc3dd
Nicholas de Araujo Figueira <79494392+NicholasBr7@users.noreply.github.com>
2022-07-27 23:01:13 -0300

    Add files via upload

---
+++ trabalho2.c
@@ -0,0 +1,107 @@
+//Aluno: Nicholas de Araujo Figueira
+//DRE: 121 088 218 
+//E-mail: nicholasaf7@gmail.com
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+//Crio o struct nó da inicio. É definido o tipo de dado e para onde cada nó aponta.
+typedef struct no{
+    float valor;
+    struct no *proximo;
+}No;
+
+//É feito o processo empilhar a pilha
+No* push(No *inicio, float numero){
+    No *novo;
+    novo = malloc(sizeof(No));
+
+    if(novo){
+        novo->valor = numero;
+        novo->proximo = inicio;
+        return novo;
+    }
+    else
+        printf("\nNao foi possivel alocar memoria!\n");
+    return NULL;
+}
+
+//É feito o processo de desempilhar a pilha
+No* pop(No **inicio){
+    No *remover;
+    remover = NULL;
+
+    if(*inicio){
+        remover = *inicio;
+        *inicio = remover->proximo;
+    }
+    else
+        printf("\nA pilha está vazia!\n");
+    return remover;
+}
+//Vai pegar os operandos e operadores e fazer as devidas operações
+float operacao(float x, float y, char z){
+
+    if (z == '+'){
+        return x + y;
+    }
+    else if (z == '-'){
+        return x - y;
+    }
+    else if (z == '*'){
+        return x * y;
+    }
+    else if (z == '/'){ 
+        return x/y;
+    }
+    else if (z == '^'){
+        return pow(x,y);
+    }
+}
+
+float calcular(char x[]){
+    char *pt;
+    float numero;
+    No *numero1, *numero2, *inicio = NULL;
+
+    //Função para quebrar um string sempre que houver um espaço.
+    pt = strtok(x, " ");
+    //Enquanto pt for diferente de nulo, vai verificar se é um sinal e passar a desempilhar números para fazer
+    //as devidas operações.
+    while(pt != NULL){
+        if(pt[0] == '+' || pt[0] == '-' || pt[0] == '/' || pt[0] == '*' || pt[0] == '^'){
+            numero1 = pop(&inicio);
+            numero2 = pop(&inicio);
+            numero = operacao(numero2->valor, numero1->valor, pt[0]);
+            inicio = push(inicio, numero);
+            free(numero1);
+            free(numero2);
+        }
+        else{
+            //Função para converter a parte numerica da string para inteiro e o resto deixa em string.
+            //O 10 é devido a base decimal.
+            numero = strtol(pt, NULL, 10);
+            inicio = push(inicio, numero);
+        }
+        pt = strtok(NULL, " ");
+    }
+    numero1 = pop(&inicio);
+    numero = numero1->valor;
+    free(numero1);
+    return numero;
+}
+
+//A main simplesmente pede para você inserir a expressão que deseja calcular seguindo essa regra como pedida.
+int main(){
+
+    char expressao[1000];
+    printf("\nEsse codigo resolve a expressao no formato notacao polonesa reversa. São aceitos +, -, *, / e ^.\n");
+    printf("\nEx.: 3 4 5 + ^ vai dar 3**9 que vale 19683.\n\n");
+    scanf("%[^\n]", expressao);
+    
+    printf("Resultado de %s: ", expressao);
+    printf("%.4f\n", calcular(expressao));
+    return 0;
+}
---
+++ trabalho3.c
@@ -0,0 +1,124 @@
+//Aluno: Nicholas de Araujo Figueira
+//DRE: 121 088 218
+//E-mail: nicholasaf7@gmail.com
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct No{
+    int valor;
+    struct No *proximo;
+} No;
+
+typedef struct{
+    No *inicio;
+} Lista;
+
+//Cria-se uma lista e aloca-se memória para ela
+Lista* gera_lista(){
+    Lista *lista = (Lista*)malloc(sizeof(No));
+    lista -> inicio = NULL;
+    return lista;
+}  
+
+//Coloca-se um valor no final da lista
+void insere_fim(Lista *lista, int valor){
+    No *no;
+    No *novo = (No *)malloc(sizeof(No));
+    novo -> valor = valor;
+    novo -> proximo = NULL;
+    //Se a lista está vazia, primeiro elemento é o "valor"
+    if(lista -> inicio == NULL){
+        lista -> inicio = novo;
+    }
+    //Percorre-se a lista até ela ficar vazia para inserir o "valor"
+    else{
+        no = lista -> inicio;
+        while(no -> proximo != NULL){
+            no = no -> proximo;
+        }
+        no -> proximo = novo;
+    }
+}
+
+int main(void){
+    int i, j = 0;
+    int vertices, arestas, posicao = 0, pos, valor_lido, qtd_chars;
+    char *linha = NULL;
+    size_t tamanho_linha = 0;
+
+    //Leitura dos dados
+
+    scanf("%d%d\n", &vertices, &arestas);
+
+    Lista * vetor_adjacente[vertices + 1];
+
+    //Gerando uma lista para cada um dos vertices
+    for(int i = 1; i <= vertices; i++){
+        vetor_adjacente[i] = gera_lista();
+    }
+    
+    //Lendo cada um dos valores conectados aos vértices
+    for(int i = 1; i <= vertices; i++){
+        getline(&linha, &tamanho_linha, stdin);
+        pos = 0, valor_lido = 0;
+        while (sscanf(&linha[pos], " %d %n", &valor_lido, &qtd_chars) > 0) {
+            pos += qtd_chars;
+            if(strlen(linha) != 0){
+                insere_fim(vetor_adjacente[i], valor_lido);
+            }
+        }
+    }
+
+    //Busca Topológica
+
+    int ordem[vertices];
+    int grau_entrada[vertices + 1];
+
+    //Todas as posições de grau de entrada passam a valer 0
+    for(int i = 1; i <= vertices; i++){
+        grau_entrada[i]=0;
+    }
+
+    No * auxiliar;
+    auxiliar = (No*) malloc(sizeof(No*));
+
+    //Calculando o grau dos vértices
+    for(int i = 1; i <= vertices; i++){
+        auxiliar = vetor_adjacente[i] -> inicio;
+        while(auxiliar != NULL){
+            grau_entrada[auxiliar -> valor]++;
+            auxiliar = auxiliar -> proximo;
+        }
+    }
+
+    //Vetor ordem pega o primeiro vértice da ordenação, ou seja, o de menor grau
+    for(int i = 1; i <= vertices; i++){
+        if(grau_entrada[i]==0){
+            ordem[posicao] = i;
+            posicao++;
+        }
+    }
+
+    //Vetor ordem recebe o restante dos vértices
+    while( j < posicao){
+        auxiliar = vetor_adjacente[ordem[j]] -> inicio;
+        while(auxiliar != NULL){
+            grau_entrada[auxiliar -> valor]--;
+            if(grau_entrada[auxiliar -> valor] == 0){
+                ordem[posicao] = auxiliar -> valor; 
+                posicao++;
+            } 
+            auxiliar = auxiliar -> proximo;
+        }
+        j++;
+    } 
+
+    //Printa o vetor ordenado 
+    printf("Resultado da ordenacao: \n");
+    for(int i = 0; i < vertices; i++){
+        printf("%d ",ordem[i]);
+    }
+    return 0;
+}
